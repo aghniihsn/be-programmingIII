0d6e592ea8722cdaa1c80d65b9dc5bdd793e5fc0
aghni <aghniihsn.gmail.com>
2025-07-07 14:29:37 +0700

    fe

--- config/cors.go
+++ config/cors.go
@@ -5,6 +5,7 @@ var allowedOrigins = []string{
 	"https://indrariksa.github.io",
 	"http://localhost:5173",
 	"http://127.0.0.1:8088/",
+	"https://fe-programming-iii.vercel.app/",
 }
 
 func GetAllowedOrigin() []string {
